2d27919a2b5891481d6774d949bc8a20cb9c036a
Sutean Rutjanalard <sutean.r@zcomsec.com>
2018-05-17 08:52:20 +0700

    Update bcast version

--- provider/time_provider.go
+++ provider/time_provider.go
@@ -2,27 +2,28 @@ package provider
 
 import (
 	"time"
-    "github.com/grafov/bcast"
+
+	"github.com/grafov/bcast"
 )
 
 func main() {
-    group := bcast.NewGroup()
-    go group.Broadcasting(0)
-    go TimeProvider(group, 100 * time.Millisecond)
-    go newReciever(group)
-    newReciever(group)
-    
+	group := bcast.NewGroup()
+	go group.Broadcast(0)
+	go TimeProvider(group, 100*time.Millisecond)
+	go newReciever(group)
+	newReciever(group)
 }
 func newReciever(group *bcast.Group) {
-        member := group.Join()
-        for {
-            println("message", member.Recv().(string))
-        }
-    }
+	member := group.Join()
+	for {
+		println("message", member.Recv().(string))
+	}
+}
+
 //TimeProvider provide time stamp every configured time
 func TimeProvider(group *bcast.Group, d time.Duration) {
-    defer group.Close()
+	defer group.Close()
 	for x := range time.Tick(d) {
 		group.Send(x.String())
-    }
+	}
 }
--- time_stream.go
+++ time_stream.go
@@ -1,39 +1,41 @@
 package main
 
 import (
-    "io"
-    "time"
-    "github.com/mossila/gin-sse-simple/provider"
-    "github.com/gin-gonic/gin"
-    "github.com/grafov/bcast"
+	"io"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/grafov/bcast"
+	"github.com/mossila/gin-sse-simple/provider"
 )
+
 type appContext struct {
-    timeGroup *bcast.Group
+	timeGroup *bcast.Group
 }
 
 func main() {
-    group := bcast.NewGroup()
-    go group.Broadcasting(0)
-    go provider.TimeProvider(group, 10 * time.Millisecond)
-    app := appContext{group}
-    
-    gin.SetMode(gin.ReleaseMode)
-    router := gin.Default()
-    router.LoadHTMLGlob("templates/*")
-    router.GET("time/", app.timeStream)
-    router.GET("client/", client)
-    router.Run(":8080")
+	group := bcast.NewGroup()
+	go group.Broadcast(0)
+	go provider.TimeProvider(group, 10*time.Millisecond)
+	app := appContext{group}
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("time/", app.timeStream)
+	router.GET("/", client)
+	router.Run(":8888")
 }
 
 func client(c *gin.Context) {
-    c.HTML(200, "index.tmpl", gin.H{})
+	c.HTML(200, "index.html", gin.H{})
 }
 
 func (app *appContext) timeStream(c *gin.Context) {
-    recv := app.timeGroup.Join()
-    defer recv.Close()
-    c.Stream(func(w io.Writer) bool {
-        c.SSEvent("message", recv.Recv().(string))
-        return true
-    })
+	recv := app.timeGroup.Join()
+	defer recv.Close()
+	c.Stream(func(w io.Writer) bool {
+		c.SSEvent("message", recv.Recv().(string))
+		return true
+	})
 }
